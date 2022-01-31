d6b5fb794836b358a375c4b1f78d9ae274e9608a
Bruno Guerra <gguerrabruno@outlook.com>
2022-01-30 22:36:44 -0300

    Adds a dialog to show coffee info

--- lib/presentation/components/coffee_list.dart
+++ lib/presentation/components/coffee_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hello_world/domain/model/coffee.dart';
+import 'package:flutter_hello_world/presentation/components/coffee_dialog.dart';
 
 /// Widget to display a list of Coffee Cards
 class CoffeeList extends StatelessWidget {
@@ -9,11 +10,28 @@ class CoffeeList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return ListView(
       children: coffees
           .map((coffee) => Card(
-                child: Text(coffee.blendName),
-                color: Colors.grey[300],
+                child: InkWell(
+                  child: Column(
+                    children: [
+                      ListTile(
+                        leading: const Icon(Icons.coffee),
+                        title: Text(coffee.blendName),
+                        subtitle: Text(coffee.origin),
+                      )
+                    ],
+                  ),
+                  onTap: () => showDialog(
+                      context: context,
+                      builder: (_) => AlertDialog(
+                            title: Text(coffee.blendName),
+                            content: CoffeeDialog(
+                              coffee: coffee,
+                            ),
+                          )),
+                ),
               ))
           .toList(),
     );
