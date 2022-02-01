d4476ae422546da6b663347b14012b76648cda52
Bruno Guerra <gguerrabruno@outlook.com>
2022-02-01 11:56:39 -0300

    Coffee data class

--- lib/domain/model/coffee.dart
+++ lib/domain/model/coffee.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class Coffee {
   final int id;
   final String uid;
@@ -6,24 +8,91 @@ class Coffee {
   final String variety;
   final String notes;
   final String intensifier;
+  Coffee({
+    required this.id,
+    required this.uid,
+    required this.blendName,
+    required this.origin,
+    required this.variety,
+    required this.notes,
+    required this.intensifier,
+  });
 
-  const Coffee(
-      {required this.id,
-      required this.uid,
-      required this.blendName,
-      required this.origin,
-      required this.variety,
-      required this.notes,
-      required this.intensifier});
-
-  factory Coffee.fromJson(Map<String, dynamic> json) {
+  Coffee copyWith({
+    int? id,
+    String? uid,
+    String? blendName,
+    String? origin,
+    String? variety,
+    String? notes,
+    String? intensifier,
+  }) {
     return Coffee(
-        id: json['id'],
-        uid: json['uid'],
-        blendName: json['blend_name'],
-        origin: json['origin'],
-        variety: json['variety'],
-        notes: json['notes'],
-        intensifier: json['intensifier']);
+      id: id ?? this.id,
+      uid: uid ?? this.uid,
+      blendName: blendName ?? this.blendName,
+      origin: origin ?? this.origin,
+      variety: variety ?? this.variety,
+      notes: notes ?? this.notes,
+      intensifier: intensifier ?? this.intensifier,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'uid': uid,
+      'blendName': blendName,
+      'origin': origin,
+      'variety': variety,
+      'notes': notes,
+      'intensifier': intensifier,
+    };
+  }
+
+  factory Coffee.fromMap(Map<String, dynamic> map) {
+    return Coffee(
+      id: map['id']?.toInt() ?? 0,
+      uid: map['uid'] ?? '',
+      blendName: map['blendName'] ?? '',
+      origin: map['origin'] ?? '',
+      variety: map['variety'] ?? '',
+      notes: map['notes'] ?? '',
+      intensifier: map['intensifier'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Coffee.fromJson(String source) => Coffee.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'Coffee(id: $id, uid: $uid, blendName: $blendName, origin: $origin, variety: $variety, notes: $notes, intensifier: $intensifier)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Coffee &&
+        other.id == id &&
+        other.uid == uid &&
+        other.blendName == blendName &&
+        other.origin == origin &&
+        other.variety == variety &&
+        other.notes == notes &&
+        other.intensifier == intensifier;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        uid.hashCode ^
+        blendName.hashCode ^
+        origin.hashCode ^
+        variety.hashCode ^
+        notes.hashCode ^
+        intensifier.hashCode;
   }
 }
